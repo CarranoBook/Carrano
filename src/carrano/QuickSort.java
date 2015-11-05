ce75cd7d94459632e806080e50dc301ee37dae14
nbleier <nbleier@T221D13LAB12.CLCILLINOIS.EDU>
2015-11-05 15:16:56 -0600

    `

--- src/carrano/QuickSort.java
+++ src/carrano/QuickSort.java
@@ -12,8 +12,20 @@ package carrano;
  */
 public class QuickSort<E extends Comparable>  {
     E[] arr;
+    final int switchToInsertion;
+    static int SWITCH_TO_INSERTION_DEFAULT = 4;
     
     public QuickSort(E[] arr) {
+        switchToInsertion = SWITCH_TO_INSERTION_DEFAULT;
+        @SuppressWarnings("unchecked")
+        E[] tempArr = (E[]) new Comparable[arr.length];
+        tempArr = arr;
+        this.arr = tempArr;
+        this.pivot(0, arr.length - 1);
+    }
+    
+    public QuickSort(E[] arr, int insert) {
+        switchToInsertion = insert;
         @SuppressWarnings("unchecked")
         E[] tempArr = (E[]) new Comparable[arr.length];
         tempArr = arr;
@@ -42,45 +54,72 @@ public class QuickSort<E extends Comparable>  {
     
     private void pivot(int begin, int end)
     {
-        int first = begin;
-        int last = end;
-        E pivot = this.selectPivot(begin, end);
-        E temp;
-        boolean check = first != last;
-        
-        while (check) {
-            //If the element in the first index is greater than the pivot
-            if ( arr[first].compareTo(pivot) > 0 )
-            {
-                temp = arr[last];
-                arr[last] = arr[first];
-                arr[first] = temp;
-                end--;
-            }
-            else {
-                first++;
-            }
-            check = first != last;
-        }//end while
-        
-        //recursive step
-        if ( first != begin && first != end) {
-            this.pivot(begin, first);
-            this.pivot(last, arr.length - 1);
+        if ( (end - begin + 1 ) <= switchToInsertion ) {
+            this.insertionSort(begin, end);
         }
+        else {
+            int first = begin;
+            int last = end;
+            E pivot = this.selectPivot(begin, end);
+            E temp;
+            boolean check = first != last;
+
+            while (check) {
+                //If the element in the first index is greater than the pivot
+                if ( arr[first].compareTo(pivot) > 0 )
+                {
+                    temp = arr[last];
+                    arr[last] = arr[first];
+                    arr[first] = temp;
+                    last--;
+                }
+                else {
+                    first++;
+                }
+                check = first != last;
+            }//end while
+
+            //recursive step
+            if ( first != begin && first != end) {
+                this.pivot(begin, first);
+                this.pivot(last, arr.length - 1);
+            }
+        } //end else
     }
+    
+    private void insertionSort(int begin, int end) {
+        E temp;
+        int minIndex;
+        for ( int i = begin; i <= end; i++ ) {
+            temp = arr[i];
+            minIndex = i;
+            for ( int j = i + 1; j <= end; j++ ) {
+                if ( temp.compareTo(arr[j]) > 0 ) {
+                    temp = arr[j];
+                    minIndex = j;
+                } //end if
+            } //end inner loop
+            if ( minIndex != i ) {
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            } //end assignment if
+        } //end outer loop
+    }
+    
     
     public E[] getArr() {
         return arr;
     }
     
     public static void main(String[] args) {
-        Integer[] ar = {8, 3, 5, 7, 6};
+        Integer[] ar = {8, 3, 5, 7, 6, 43, 1234, 1234 ,3 ,4,3};
         QuickSort test = new QuickSort(ar);
-        @SuppressWarnings("Unchecked")
-        Integer[] tempArr = (Integer[]) new Comparable[ar.length];
-        ar =tempArr;
-        for ( Integer x : ar ) {
+        
+        @SuppressWarnings("unchecked")
+        Integer[] tempArr = (Integer[]) new QuickSort(ar).getArr();
+        
+
+        for ( Integer x : tempArr ) {
             System.out.println(x);
         }
     }
